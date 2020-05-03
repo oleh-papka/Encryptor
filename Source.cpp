ec1fb3e862ebfc07c0747298d15282c0bc7c1cce
Oleg Papka <60508074+OlegPapka2@users.noreply.github.com>
2020-05-03 23:38:18 +0300

    Added reading and writing

--- Source.cpp
+++ Source.cpp
@@ -1,14 +1,32 @@
 #include <stdio.h>
 #include <iostream>
+#include <fstream>
+
+using namespace std;
 
 /*
 	Program for encrypting and decrypting data
 	master branch
 */
 
-int main() {
+char text[200];
 
-	std::cout << "Hello World!";
+int main() {
+	fstream read_file;
+	fstream write_file;
+
+	read_file.open("Data.txt", ios::in);
+	write_file.open("Results.txt", ios::out);
+
+	std::cout << "What to write to file?" << std::endl;
+	cin.getline(text, sizeof(text));
+
+	write_file << text << endl;
+
+	std::cout << "Was writen:" << text << std::endl;
+
+	read_file.close();
+	write_file.close();
 
 	return 0;
 }
